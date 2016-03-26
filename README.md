ccaeaaf71fa44b4c08cfb2961ab1868096980e39
hentai-mew <wscl124914@gmail.com>
2016-03-27 03:46:45 +0800

    fix bug

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Groovy环境:
 ````shell
 wget https://raw.githubusercontent.com/hentai-mew/linux_shells/master/groovy.sh -O - | sh
 ````
+Node环境:
+````shell
+wget https://raw.githubusercontent.com/hentai-mew/linux_shells/master/node.sh -O - | sh
+````
--- node.sh
+++ node.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-VERSION=2.4.6
-FILE_NAME=apache-groovy-binary-$VERSION.zip
-SOURCE_DIR=groovy-$VERSION
-TARGET_DIR=groovy
-DL_ADDRESS=https://dl.bintray.com/groovy/maven/$FILE_NAME
+VERSION=5.9.1
+FILE_NAME=node-v$VERSION-linux-x64.tar.xz
+SOURCE_DIR=node-v$VERSION-linux-x64
+TARGET_DIR=node
+DL_ADDRESS=https://nodejs.org/dist/v$VERSION/$FILE_NAME
 SOFT_DIR=/soft
 if [ ! -e $SOFT_DIR ];then
 echo $SOFT_DIR dir is not exists.
@@ -12,12 +12,12 @@ exit
 else
 wget -O $SOFT_DIR/$FILE_NAME $DL_ADDRESS
 #tgz & rename
-(cd $SOFT_DIR && unzip $FILE_NAME && mv $SOURCE_DIR $TARGET_DIR)
+(cd $SOFT_DIR && tar -Jxf $FILE_NAME && mv $SOURCE_DIR $TARGET_DIR)
 #env
-sed -i '/groovy_/Id' /etc/profile
-echo "#Groovy_env
-GROOVY_HOME=$SOFT_DIR/$TARGET_DIR
-PATH=\$GROOVY_HOME/bin:\$PATH
+sed -i '/node_/Id' /etc/profile
+echo "#Node_env
+NODE_HOME=$SOFT_DIR/$TARGET_DIR
+PATH=\$NODE_HOME/bin:\$PATH
 export PATH GROOVY_HOME
 " >> /etc/profile
 source /etc/profile
