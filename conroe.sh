38fd75df1f3f9c6ca50fa8308eaacf465afd0026
hentai-mew <wscl124914@gmail.com>
2016-04-09 04:36:17 +0800

    update conroe boot sh.

--- conroe.sh
+++ conroe.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [ ! doceutil ];then
-echo doceutil not installed.
+if [ ! dkutil ];then
+echo dkutil not installed.
 exit
 else
 for i in $(seq 1 4)
 do
-doceutil run -v /soft/node:/usr/local/node -v /apps/conroe:/src/app \
--p 300${i}:3000 --name conroe-${i} -d wscl124914/node_runtime
+dkutil run -v /apps/conroe:/app \
+-p 300${i}:3000 --name conroe-${i} -d bluerain/node:express
 done
 fi
