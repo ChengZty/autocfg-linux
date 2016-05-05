199b64989ccb5022b3d4d41d978784ebe1ce43f2
hentai-mew <wscl124914@gmail.com>
2016-05-06 03:52:56 +0800

    update golang.

--- go.sh
+++ go.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
 
-VERSION=1.6
-FILE_NAME=go$VERSION.linux-amd64.tar.gz
-DL_ADDRESS=https://storage.googleapis.com/golang/$FILE_NAME
+VERSION=1.6.2
+FILE_NAME=go${VERSION}.linux-amd64.tar.gz
+DL_ADDRESS=https://storage.googleapis.com/golang/${FILE_NAME}
 SOURCE_DIR=go
 TARGET_DIR=go
 SOFT_DIR=/soft
 MAIN_ENV_HOME=GOROOT
-GOPATH=$SOFT_DIR/GOPATH
-if [ ! -e $SOFT_DIR ];then
-echo $SOFT_DIR dir is not exists.
+GOPATH=${SOFT_DIR}/GOPATH
+if [ ! -e ${SOFT_DIR} ];then
+echo ${SOFT_DIR} dir is not exists.
 exit
 else
-wget -O $SOFT_DIR/$FILE_NAME $DL_ADDRESS
-#tgz & rename
-(cd $SOFT_DIR && tar -zxvf $FILE_NAME)
+wget -O ${SOFT_DIR}/${FILE_NAME} ${DL_ADDRESS}
+#rm old version
+rm -rf ${SOFT_DIR}/${TARGET_DIR}
+#tgr & rename
+(cd ${SOFT_DIR} && tar -zxvf ${FILE_NAME})
 #env
-if [ ! -e $GOPATH ];then
-mkdir $GOPATH -p
+if [ ! -e ${GOPATH} ];then
+mkdir ${GOPATH} -p
 fi
 sed -i '/${TARGET_DIR}_/Id' /etc/profile
-sed -i '/$MAIN_ENV_HOME/Id' /etc/profile
+sed -i '/${MAIN_ENV_HOME}/Id' /etc/profile
 sed -i '/GOPATH/Id' /etc/profile
 echo "#${TARGET_DIR}_env
 $MAIN_ENV_HOME=$SOFT_DIR/$TARGET_DIR
 GOPATH=$GOPATH
 PATH=\$${MAIN_ENV_HOME}/bin:\$GOPATH/bin:\$PATH
-export PATH $MAIN_ENV_HOME GOPATH
+export PATH ${MAIN_ENV_HOME} GOPATH
 " >> /etc/profile
 source /etc/profile
 fi
