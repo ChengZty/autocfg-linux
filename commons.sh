cc5d68a616c8a6be1e34ea6a7021cc097a25e64a
hentai-mew <me@bluerain.io>
2016-10-08 12:49:10 +0800

    完成版本。

--- commons.sh
+++ commons.sh
@@ -3,16 +3,30 @@
 # 公共 BashShell 支持大量环境的自动化配置
 # arg01 -> 环境的名称
 # arg02 -> 安装到目录
+# arg03 -> UID:GID(权限)
 #########################
 # Java       -> java    #
-# Go         -> go      # 目
-# Groovy     -> groovy  # 前
-# Node       -> node    # 包
-# Maven      -> maven   # 括
+# Node       -> node    #
+# Go         -> golang  #
+# Groovy     -> groovy  #
+# Grails     -> grails  #
+# Maven      -> maven   #
+# Gradle     -> gradle  #
 # AndroidSdk -> android #
+# Hadoop     -> hadoop  #
 #########################
 # 当前为开发(Dev)版本
 # 推荐使用release版本
+
+V_JDK=8u91
+V_NODE=4.6.0
+V_GO=1.7.1
+V_GROOVY=2.4.7
+V_GRAILS=3.2.0
+V_MAVEN=3.3.9
+V_GRADLE=3.0
+V_ANDROID_SDK=24.4.1
+V_HADOOP=3.7.3
 
 # Dev Started:
 # 定义（接受传参）常量
@@ -45,6 +59,7 @@ common-dl() {
 SUFFIX_TAR='tar'
 SUFFIX_TAR_GZ='tar.gz'
 SUFFIX_TAR_XZ='tar.xz'
+SUFFIX_ZIP='zip'
 common-unzip() {
     suffix=$1; dir_name=$2
     home_path=${SOFT_DIR}/${dir_name}
@@ -62,6 +77,8 @@ common-unzip() {
         ${SUFFIX_TAR_GZ}) bak && tar -zxvf ${DL_FILE} -C ${home_path} && mv_my_dir
         ;;
         ${SUFFIX_TAR_XZ}) bak && tar -Jxf ${DL_FILE} -C ${home_path} && mv_my_dir
+        ;;
+        ${SUFFIX_ZIP}) bak && unzip ${DL_FILE} -d ${home_path} && mv_my_dir
         ;;
         *) echo \*
     esac
@@ -82,37 +99,12 @@ common-set-profile() {
 }
 # Environment Started.
 
-# Test环境
-test_env() {
-    # 0.1 定义变量: 下载地址
-    ADDRESS='http://dl.bluerain.io/test.tar.gz'
-    # 0.2 定义变量: 保存文件名
-    SAVE_NAME='test.tar.gz.1'
-    # 0.3 定义变量: 解压目录名
-    DIR_NAME='test'
-    # 0.4 定义变量(数组): 删除旧环境变量关键字
-    ENV_KEYS=('test_')
-    # 1.下载
-    common-dl ${ADDRESS} ${SAVE_NAME}
-    # 2.解压
-    common-unzip ${SUFFIX_TAR_GZ} ${DIR_NAME}
-    # 3.配置环境变量（永久性）
-    # 3.1这里一直搞不定写入换行问题，所以需要自己实现写入方法!!
-    write() {
-        echo "#Test_env
-TEST_HOME=${home_path}
-PATH=\$TEST_HOME/:\$PATH
-export TEST_HOME" >> /etc/profile
-    }
-    # 3.2 调用函数完成环境变量配置
-    common-set-profile
-}
 # Java环境
 java_env() {
     # 0.1 定义变量: 下载地址
-    ADDRESS='http://bridsystems.net/downloads/java/jdk-8u91-linux-x64.tar.gz'
+    ADDRESS="http://bridsystems.net/downloads/java/jdk-${V_JDK}-linux-x64.tar.gz"
     # 0.2 定义变量: 保存文件名
-    SAVE_NAME='jdk.tar.gz'
+    SAVE_NAME="jdk${V_JDK}.tar.gz"
     # 0.3 定义变量: 解压目录名
     DIR_NAME='jdk'
     # 0.4 定义变量(数组): 删除旧环境变量关键字
@@ -122,7 +114,7 @@ java_env() {
     # 2.解压
     common-unzip ${SUFFIX_TAR_GZ} ${DIR_NAME}
     # 3.配置环境变量（永久性）
-    # 3.1这里一直搞不定写入换行问题，所以需要自己实现写入方法!!
+    # 3.1这里一直搞不定写入换行问题，所以需要自己实现write函数!!
     write() {
         echo "#Java_env
 JAVA_HOME=${home_path}
@@ -137,9 +129,9 @@ export JAVA_HOME CLASSPATH" >> /etc/profile
 # Node环境
 node_env() {
     # 0.1 定义变量: 下载地址
-    ADDRESS='https://nodejs.org/dist/v4.6.0/node-v4.6.0-linux-x64.tar.xz'
+    ADDRESS="https://nodejs.org/dist/v${V_NODE}/node-v${V_NODE}-linux-x64.tar.xz"
     # 0.2 定义变量: 保存文件名
-    SAVE_NAME='node.tar.xz'
+    SAVE_NAME="node${V_NODE}.tar.xz"
     # 0.3 定义变量: 解压目录名
     DIR_NAME='node'
     # 0.4 定义变量(数组): 删除旧环境变量关键字
@@ -149,7 +141,7 @@ node_env() {
     # 2.解压
     common-unzip ${SUFFIX_TAR_XZ} ${DIR_NAME}
     # 3.配置环境变量（永久性）
-    # 3.1这里一直搞不定写入换行问题，所以需要自己实现写入方法!!
+    # 3.1这里一直搞不定写入换行问题，所以需要自己实现write函数!!
     write() {
         echo "#Node_env
 NODE_HOME=${home_path}
@@ -159,7 +151,184 @@ export NODE_HOME" >> /etc/profile
     # 3.2 调用函数完成环境变量配置
     common-set-profile
 }
-
+# GoLang环境
+golang_env() {
+    # 0.1 定义变量: 下载地址
+    ADDRESS="https://storage.googleapis.com/golang/go${V_GO}.linux-amd64.tar.gz"
+    # 0.2 定义变量: 保存文件名
+    SAVE_NAME="go${V_GO}.tar.gz"
+    # 0.3 定义变量: 解压目录名
+    DIR_NAME='go'
+    # 0.4 定义变量(数组): 删除旧环境变量关键字
+    ENV_KEYS=('go_env' 'GOROOT' 'GOPATH')
+    # 1.下载
+    common-dl ${ADDRESS} ${SAVE_NAME}
+    # 2.解压
+    common-unzip ${SUFFIX_TAR_GZ} ${DIR_NAME}
+    # 2.1 创建GOPATH
+    if [ ! -e ${SOFT_DIR}/GOPATH ];then mkdir ${SOFT_DIR}/GOPATH;fi
+    # 3.配置环境变量（永久性）
+    # 3.1这里一直搞不定写入换行问题，所以需要自己实现write函数!!
+    write() {
+        echo "#Go_env
+GOROOT=${home_path}
+GOPATH=${SOFT_DIR}/GOPATH
+PATH=\$GOROOT/bin:\$GOPATH/bin:\$PATH
+export GOROOT GOPATH" >> /etc/profile
+    }
+    # 3.2 调用函数完成环境变量配置
+    common-set-profile
+}
+# Groovy环境
+groovy_env() {
+    # 0.1 定义变量: 下载地址
+    ADDRESS="https://dl.bintray.com/groovy/maven/apache-groovy-binary-${V_GROOVY}.zip"
+    # 0.2 定义变量: 保存文件名
+    SAVE_NAME="groovy${V_GROOVY}.zip"
+    # 0.3 定义变量: 解压目录名
+    DIR_NAME='groovy'
+    # 0.4 定义变量(数组): 删除旧环境变量关键字
+    ENV_KEYS=('groovy_')
+    # 1.下载
+    common-dl ${ADDRESS} ${SAVE_NAME}
+    # 2.解压
+    common-unzip ${SUFFIX_ZIP} ${DIR_NAME}
+    # 3.配置环境变量（永久性）
+    # 3.1这里一直搞不定写入换行问题，所以需要自己实现write函数!!
+    write() {
+        echo "#Groovy_env
+GROOVY_HOME=${home_path}
+PATH=\$GROOVY_HOME/bin:\$PATH
+export GROOVY_HOME" >> /etc/profile
+    }
+    # 3.2 调用函数完成环境变量配置
+    common-set-profile
+}
+# Grails环境
+grails_env() {
+    # 0.1 定义变量: 下载地址
+    ADDRESS="https://github.com/grails/grails-core/releases/download/v${V_GRAILS}/grails-${V_GRAILS}.zip"
+    # 0.2 定义变量: 保存文件名
+    SAVE_NAME="grails${V_GRAILS}.zip"
+    # 0.3 定义变量: 解压目录名
+    DIR_NAME='grails'
+    # 0.4 定义变量(数组): 删除旧环境变量关键字
+    ENV_KEYS=('grails_')
+    # 1.下载
+    common-dl ${ADDRESS} ${SAVE_NAME}
+    # 2.解压
+    common-unzip ${SUFFIX_ZIP} ${DIR_NAME}
+    # 3.配置环境变量（永久性）
+    # 3.1这里一直搞不定写入换行问题，所以需要自己实现write函数!!
+    write() {
+        echo "#Grails_env
+GRAILS_HOME=${home_path}
+PATH=\$GRAILS_HOME/bin:\$PATH
+export GRAILS_HOME" >> /etc/profile
+    }
+    # 3.2 调用函数完成环境变量配置
+    common-set-profile
+}
+# Maven环境
+maven_env() {
+    # 0.1 定义变量: 下载地址
+    ADDRESS="http://mirrors.cnnic.cn/apache/maven/maven-3/${V_MAVEN}/binaries/apache-maven-${V_MAVEN}-bin.tar.gz"
+    # 0.2 定义变量: 保存文件名
+    SAVE_NAME="maven${V_MAVEN}.tar.gz"
+    # 0.3 定义变量: 解压目录名
+    DIR_NAME='maven'
+    # 0.4 定义变量(数组): 删除旧环境变量关键字
+    ENV_KEYS=('maven_' 'm2_')
+    # 1.下载
+    common-dl ${ADDRESS} ${SAVE_NAME}
+    # 2.解压
+    common-unzip ${SUFFIX_TAR_GZ} ${DIR_NAME}
+    # 3.配置环境变量（永久性）
+    # 3.1这里一直搞不定写入换行问题，所以需要自己实现write函数!!
+    write() {
+        echo "#Maven_env
+M2_HOME=${home_path}
+PATH=\$M2_HOME/bin:\$PATH
+export M2_HOME" >> /etc/profile
+    }
+    # 3.2 调用函数完成环境变量配置
+    common-set-profile
+}
+# Gradle环境
+gradle_env() {
+    # 0.1 定义变量: 下载地址
+    ADDRESS="https://services.gradle.org/distributions/gradle-${V_GRADLE}-all.zip"
+    # 0.2 定义变量: 保存文件名
+    SAVE_NAME="gradle${V_GRADLE}.zip"
+    # 0.3 定义变量: 解压目录名
+    DIR_NAME='gradle'
+    # 0.4 定义变量(数组): 删除旧环境变量关键字
+    ENV_KEYS=('gradle_')
+    # 1.下载
+    common-dl ${ADDRESS} ${SAVE_NAME}
+    # 2.解压
+    common-unzip ${SUFFIX_ZIP} ${DIR_NAME}
+    # 3.配置环境变量（永久性）
+    # 3.1这里一直搞不定写入换行问题，所以需要自己实现write函数!!
+    write() {
+        echo "#Gradle_env
+GRADLE_HOME=${home_path}
+PATH=\$GRADLE_HOME/bin:\$PATH
+export GRADLE_HOME" >> /etc/profile
+    }
+    # 3.2 调用函数完成环境变量配置
+    common-set-profile
+}
+# Android环境
+android_env() {
+    # 0.1 定义变量: 下载地址
+    ADDRESS="https://dl.google.com/android/android-sdk_r${V_ANDROID_SDK}-linux.tgz"
+    # 0.2 定义变量: 保存文件名
+    SAVE_NAME="android-sdk${V_ANDROID_SDK}.tgz"
+    # 0.3 定义变量: 解压目录名
+    DIR_NAME='android-sdk'
+    # 0.4 定义变量(数组): 删除旧环境变量关键字
+    ENV_KEYS=('android_')
+    # 1.下载
+    common-dl ${ADDRESS} ${SAVE_NAME}
+    # 2.解压
+    common-unzip ${SUFFIX_TAR_GZ} ${DIR_NAME}
+    # 3.配置环境变量（永久性）
+    # 3.1这里一直搞不定写入换行问题，所以需要自己实现write函数!!
+    write() {
+        echo "#Android_env
+ANDROID_SDK_HOME=${home_path}
+PATH=\$ANDROID_SDK_HOME/platform-tools:\$ANDROID_SDK_HOME/tools:\$PATH
+export ANDROID_SDK_HOME" >> /etc/profile
+    }
+    # 3.2 调用函数完成环境变量配置
+    common-set-profile
+}
+# Hadoop环境
+hadoop_env() {
+    # 0.1 定义变量: 下载地址
+    ADDRESS="http://www.apache.org/dyn/closer.cgi/hadoop/common/hadoop-${V_HADOOP}/hadoop-${V_HADOOP}.tar.gz"
+    # 0.2 定义变量: 保存文件名
+    SAVE_NAME="hadoop${V_HADOOP}.tar.gz"
+    # 0.3 定义变量: 解压目录名
+    DIR_NAME='hadoop'
+    # 0.4 定义变量(数组): 删除旧环境变量关键字
+    ENV_KEYS=('hadoop_')
+    # 1.下载
+    common-dl ${ADDRESS} ${SAVE_NAME}
+    # 2.解压
+    common-unzip ${SUFFIX_TAR_GZ} ${DIR_NAME}
+    # 3.配置环境变量（永久性）
+    # 3.1这里一直搞不定写入换行问题，所以需要自己实现write函数!!
+    write() {
+        echo "#Hadoop_env
+HADOOP_HOME=${home_path}
+PATH=\$HADOOP_HOME/bin:\$PATH
+export HADOOP_HOME" >> /etc/profile
+    }
+    # 3.2 调用函数完成环境变量配置
+    common-set-profile
+}
 # 入口环境
 main() {
     # 调用对应环境函数
